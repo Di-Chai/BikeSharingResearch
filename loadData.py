665371052eff52df903f466362d674b45623fd37
Di-Chai <31341602+Di-Chai@users.noreply.github.com>
2017-12-04 06:21:20 -0500

    Add files via upload

--- loadData.py
+++ loadData.py
@@ -1,18 +1,28 @@
-from localPath import csvDataPath
+from localPath import *
+from utils.getJsonData import getJsonData
 import os
 import csv
 
-csvFileNameList = [e for e in os.listdir(csvDataPath) if e.endswith(".csv")]
+def load_all_csv_data():
+    csvFileNameList = [e for e in os.listdir(csvDataPath) if e.endswith(".csv")]
+    # This will take a long time ...
+    allRecords = []
+    overAllRecordNumber = 0
+    for csvFile in csvFileNameList:
+        with open(os.path.join(csvDataPath, csvFile)) as f:
+            print(csvFile)
+            f_csv = csv.reader(f)
+            headers = next(f_csv)
+            for row in f_csv:
+                overAllRecordNumber += 1
+                allRecords.append(row)
+    print('overAllRecordNumber :', overAllRecordNumber)
+    return allRecords
 
-# This will take a long time ...
-allRecords = []
-overAllRecordNumber = 0
-for csvFile in csvFileNameList:
-    with open(os.path.join(csvDataPath, csvFile)) as f:
-        print(csvFile)
-        f_csv = csv.reader(f)
-        headers = next(f_csv)
-        for row in f_csv:
-            overAllRecordNumber += 1
-            # allRecords.append(row)
-print(overAllRecordNumber)
+def get_stationIDList():
+    stationIDOrderByBuildTime = getJsonData('stationIdOrderByBuildTime.json')
+    return stationIDOrderByBuildTime['stationID']
+
+
+
+get_stationIDList()
--- localPath.py
+++ localPath.py
@@ -7,3 +7,14 @@ csvDataPath = os.path.join(projectPath, 'csvData/')
 htmlPath = os.path.join(projectPath, 'html')
 
 jsonPath = os.path.join(projectPath, 'json')
+
+demandDataPath = os.path.join(projectPath, 'demandData')
+
+pngPath = os.path.join(projectPath, 'png')
+
+ObviousPngPath = os.path.join(pngPath, 'Obvious')
+
+ReversePngPath = os.path.join(pngPath, 'Reverse')
+
+UnchangedPngPath = os.path.join(pngPath, 'Unchanged')
+
